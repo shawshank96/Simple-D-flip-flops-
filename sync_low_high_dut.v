ae6f86a6deade2da45730ba0b1a4db9ddd49afb3
Shashank Shivashankar <60277247+shawshank96@users.noreply.github.com>
2020-08-31 12:43:33 -0700

    Update and rename dff3_dut.v to sync_low_high_dut.v

--- sync_low_high_dut.v
+++ sync_low_high_dut.v
@@ -1,3 +1,6 @@
+//Code by Shashank Shivashankar - 08/29/2020
+//Synchronous active LOW reset and active HIGH enable D-flip flop
+
 module dff3_dut(d, clk, rst, en, q);
 input d, clk, rst, en;
 output q;
@@ -19,6 +22,7 @@ begin
 		else
 		begin
 		q <= q;
-		end	end
+		end
+	end
 end
 endmodule
